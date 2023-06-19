8fd39f41b2d7ea576ed0549edd45f345f592b83b
Drewster0128 <acook21@hawk.iit.edu>
2023-06-19 14:06:16 -0500

    Create student table in database

---
+++ student.sql
@@ -0,0 +1,15 @@
+USE student;
+
+DROP TABLE student;
+
+CREATE TABLE student (
+    aNumber CHAR(9),
+    fName VARCHAR(25),
+    lName VARCHAR(25),
+    email VARCHAR(25),
+    username VARCHAR(25),
+    password VARCHAR(25),
+    PRIMARY KEY (aNumber)
+);
+
+INSERT INTO student (aNumber, fName, lName, email, username, password) VALUES ("20457959", "Andrew", "Cook", "acook21@hawk.iit.edu", "acook21", "testPassword");
